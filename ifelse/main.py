94d28b2459e3ca67a730cc0d707f08575f963f33
studdw <lucaskaftanp@gmail.com>
2026-03-19 20:29:28 -0300

    Melhora na visualização dos itens

--- ifelse/main.py
+++ ifelse/main.py
@@ -9,8 +9,8 @@ elif 1621 < salario <= 2902.85:
     salariox = salario - 1621 #Valor do salario diante a segunda faixa
     aliquota2 = salariox*9/100 #Valor da segunda faixa (x)
     print(f"""O valor dos descontos ficaram assim|
-Faixa 1 = {aliquota}
-Faixa 2 = {aliquota2}
+Faixa 1 = {aliquota:.2f}
+Faixa 2 = {aliquota2:.2f}
 Ficando com um valor total de descontos de {aliquota + aliquota2:.2f}
 Seu salario ficou de: {salario - (aliquota+aliquota2):.2f}""")
 
@@ -20,9 +20,9 @@ elif 2902.85 < salario <= 4354.27:
     salariox = salario-2902.85 # Valor do salario diante a terceira faixa
     aliquota3 = salariox * 12/100 # Valor da terceira faixa (x)
     print(f"""O valor dos descontos ficaram assim|
-Faixa 1 = {aliquota}
-Faixa 2 = {aliquota2}
-Faixa 3 = {aliquota3}
+Faixa 1 = {aliquota:.2f}
+Faixa 2 = {aliquota2:.2f}
+Faixa 3 = {aliquota3:.2f}
 Ficando com um valor total de descontos de {aliquota+aliquota2+aliquota3:.2f}
 Seu salario ficou de: {salario - (aliquota+aliquota2+aliquota3):.2f}""")
 
@@ -33,10 +33,10 @@ elif 4354.27 < salario <= 8475.55:
     salariox = salario-4354.27 #Valor do salario diante a quarta faixa
     aliquota4 = salariox *14/100 # Valor da quarta faixa (x)
     print(f"""O valor dos descontos ficaram assim|
-Faixa 1 = {aliquota}
-Faixa 2 = {aliquota2}
-Faixa 3 = {aliquota3}
-Faixa 4 = {aliquota4}
+Faixa 1 = {aliquota:.2f}
+Faixa 2 = {aliquota2:.2f}
+Faixa 3 = {aliquota3:.2f}
+Faixa 4 = {aliquota4:.2f}
 Ficando com um valor total de descontos de {aliquota+aliquota2+aliquota3+aliquota4:.2f}
 Seu salario ficou de: {salario - (aliquota+aliquota2+aliquota3+aliquota4):.2f}""")
 
