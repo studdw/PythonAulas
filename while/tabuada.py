2658160f78939eebeb92060ca5af0cb187c7d98b
studdw <lucaskaftanp@gmail.com>
2026-03-26 22:35:40 -0300

    -

---
+++ while/tabuada.py
@@ -0,0 +1,10 @@
+n = int(input("Escreva um numero: "))
+
+i = 1
+
+while i<=10:
+    print(f"{n} X {i} = {n*i}")
+    i = i+1
+
+print("Fim!")
+
---
+++ while/tabuada2.py
@@ -0,0 +1,7 @@
+n = int(input("Digite o numero n: "))
+
+i=1
+
+while i<=n:
+    print(i)
+    i=i+1
