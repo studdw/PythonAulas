df1efbd45f427e67db0200b9e35f6d2d48f1177b
studdw <lucaskaftanp@gmail.com>
2026-04-04 20:04:24 -0300

    correção ex1

--- cp1-python/ex1.py
+++ cp1-python/ex1.py
@@ -1,54 +1,30 @@
 # Atividade de calculo de consumo
 
 mes_anterior = float(input("Qual foi o volume mensal do mês anterior(m³)? "))
-mes_atual = float(input("Qual foi volume mensal do mês atual(m³)? "))
+mes_atual = float(input("Qual foi o volume mensal do atual(m³)? "))
 
 if mes_atual <= 20:
-    if mes_anterior>mes_atual:
-        valor = mes_atual * 2
-        multa = valor * 10 / 100
-        total = valor + multa
-        print(f"Sua conta ficou no total de: {total}")
-    else:
-        valor = mes_atual*2
-        desconto = valor * 15/100
-        total = valor - desconto
-        print(f"Sua conta ficou no total de {total}")
+    valor = mes_atual * 2
 
-
-elif mes_atual > 20 and mes_atual<=35:
-    if mes_anterior > mes_atual:
-        valor = mes_atual * 3.5
-        multa = valor * 10/100
-        total = valor + multa
-        print(f"Sua conta ficou no total de: {total}")
-    else:
-        valor = mes_atual*3.5
-        desconto = valor * 15 / 100
-        total = valor - desconto
-        print(f"Sua conta ficou no total de {total}")
+elif mes_atual > 20 and mes_atual <= 35:
+    valor = mes_atual * 3.5
 
 elif mes_atual > 35 and mes_atual <= 50:
-    if mes_anterior > mes_atual:
-        valor = mes_atual * 5.5
-        multa = valor * 10 / 100
-        total = valor + multa
-        print(f"Sua conta ficou no total de: {total}")
-    else:
-        valor = mes_atual*5.5
-        desconto = valor * 15 / 100
-        total = valor - desconto
-        print(f"Sua conta ficou no total de {total}")
+    valor = mes_atual * 5.5
 
 else:
-    if mes_anterior>mes_atual:
-        valor = mes_atual * 7
-        multa = valor * 10 / 100
-        total = valor + multa
-        print(f"Sua conta ficou no total de: {total}")
-    else:
-        valor = mes_atual*5.5
-        desconto = valor * 15 / 100
-        total = valor - desconto
-        print(f"Sua conta ficou no total de {total}")
+    valor = mes_atual * 7
+
+# Regra de desconto ou multa
+if mes_atual < mes_anterior:
+    desconto = valor * 15 / 100
+    total = valor - desconto
+    print(f"Desconto: {desconto:.2f}")
+else:
+    multa = valor * 10 / 100
+    total = valor + multa
+    print(f"Multa: {multa:.2f}")
+
+print(f"Valor do consumo: {valor:.2f}")
+print(f"Total da conta: {total:.2f}")
 
