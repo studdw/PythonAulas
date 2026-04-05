1e3f76832ebca592c98845e2f2e92e6656e877f0
studdw <lucaskaftanp@gmail.com>
2026-04-05 18:13:12 -0300

    Correções pequenas

--- cp1-python/ex4.py
+++ cp1-python/ex4.py
@@ -1,11 +1,19 @@
-#Par e Impar
-
 num = int(input("Quantos numeros deseja digitar? "))
+
+pares = 0
+impares = 0
 
 while num > 0:
     valor = int(input("Digite um numero: "))
+    
     if valor % 2 == 0:
         print("Par")
+        pares += 1
     else:
         print("Impar")
+        impares += 1
+    
     num -= 1
+
+print(f"Total de pares: {pares}")
+print(f"Total de impares: {impares}")
--- cp1-python/ex5.py
+++ cp1-python/ex5.py
@@ -1,5 +1,3 @@
-#Urna
-
 print("""
 1 - Candidato A
 2 - Candidato B
@@ -8,34 +6,34 @@ print("""
 0 - Encerrar votação
 ==================================================""")
 
-voto = int(input("Digite seu voto: "))
 total1 = 0
 total2 = 0
 total3 = 0
 total4 = 0
 
+voto = int(input("Digite seu voto: "))
+
 while voto != 0:
     if voto == 1:
         print("Voto para o candidato A")
         total1 += 1
-        voto = int(input("Digite seu voto: "))
     elif voto == 2:
         print("Voto para o candidato B")
         total2 += 1
-        voto = int(input("Digite seu voto: "))
     elif voto == 3:
         print("Voto nulo")
         total3 += 1
-        voto = int(input("Digite seu voto: "))
     elif voto == 4:
         print("Voto em branco")
-        total4
-        voto
+        total4 += 1
     else:
         print("Voto inválido!")
-        voto = int(input("Digite seu voto: "))
-    print(f"""O candidato A tem {total1} votos
-O candidato B tem {total2} votos
-Teve {total3} votos nulos
-Teve {total4} votos em branco
+
+    voto = int(input("Digite seu voto: "))
+
+print(f"""
+Candidato A: {total1}
+Candidato B: {total2}
+Votos nulos: {total3}
+Votos em branco: {total4}
 """)
