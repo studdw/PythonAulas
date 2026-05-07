7e910b7fc405464e264a0868e7ff9a8f28a68079
studdw <lucaskaftanp@gmail.com>
2026-05-07 19:42:34 -0300

    .

--- tuplas/tuplas.py
+++ tuplas/tuplas.py
@@ -1,3 +1,8 @@
-semana = ("segunda","terça","quarta","quinta","sexta","sabado", "domingo")
-for c in semana:
-    print(c)
+grupo = ("México", "Republica Tcheca", "Coréia do Sul", "África do Sul")
+
+def confrontos(grupo):
+    for i in range(len(grupo)):
+        for j in range(i + 1, len(grupo)):
+            print(f"{grupo[i]} X {grupo[j]}")
+
+confrontos(grupo)
